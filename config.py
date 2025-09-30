04b8d95417c8a987c5ab0bc2ddc2dc8cc8d99be4
BALM <106144251+BlamzKunG@users.noreply.github.com>
2025-09-30 12:07:38 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 # Token ของ bot
 TOKEN = "token"
-POLL_INTERVAL = 0.5
+POLL_INTERVAL = 15
 # รูปแบบเกม (สำหรับตอนทดสอบเราใช้ 'unlimited')
 # Lichess API จะส่ง challenge มาแล้ว bot จะตอบรับอัตโนมัติ
 GAME_VARIANT = "unlimited"  # ไม่จำกัดเวลา, casual
+
 
